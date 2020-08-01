740548ab2980b85dfb23f41fc00425413b79817d
swhan0329 <46466469+swhan0329@users.noreply.github.com>
2020-08-01 21:30:23 +0900

    add incruit hompage jobs and add file for saving all jobs to a csv file

--- linkedin.py
+++ linkedin.py
@@ -1,15 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
-LIMIT = 50
-URL = f"https://www.indeed.com/jobs?q=machine+learning&limit={LIMIT}"
+LIMIT = 20
+URL = f"http://search.incruit.com/list/search.asp?col=job&il=y&kw=machine+learning"
 
 def get_last_page():
     result = requests.get(URL)
-
     soup = BeautifulSoup(result.text, "html.parser")
-    pagination = soup.find("div",{"class":"pagination"})
-
+    pagination = soup.find("p",{"class":"sqr_paging sqr_pg_mid"})
     links = pagination.find_all('a')
     pages = []
     for link in links[:-1]:
@@ -20,8 +18,8 @@ def get_last_page():
     return max_page
 
 def extract_jobs(html):
-    title = html.find('h2').find('a')["title"]
-    company = html.find("span", {"class": "company"})
+    title = html.find("span", {"class": "rcrtTitle"}).text
+    company = html.find("h3")
     if company:
         company_ancor = company.find("a")
         if company_ancor is not None:
@@ -31,17 +29,18 @@ def extract_jobs(html):
         company = company.strip()
     else:
         company = None
-    location = html.find("div", {"class": "recJobLoc"})['data-rc-loc']
-    job_id = html['data-jk']
-    return {'title':title, 'company':company, 'location':location,'link':f"https://www.indeed.com/viewjob?jk={job_id}"}
+    location = html.find("p", {"class": "etc"}).find('span').string.split('|')[1]
+    job_id_html = html.find("p", {"class": "detail"}).find('button')
+    job_id = job_id_html['f_job']
+    return {'title':title, 'company':company, 'location':location,'link':f"http://job.incruit.com/jobdb_info/jobpost.asp?job={job_id}"}
 
 def get_jobs(last_pages):
     jobs = []
     for page in range(last_pages):
         print(f"Scrapping page {page}")
-        result = requests.get(f"{URL}&start={page*LIMIT}")
+        result = requests.get(f"{URL}&startno={page*LIMIT}")
         soup = BeautifulSoup(result.text, "html.parser")
-        job_results = soup.find_all("div", {"class": "jobsearch-SerpJobCard"})
+        job_results = soup.find("ul", {"class": "litype01"}).find_all("li")
         for job_result in job_results:
             job = extract_jobs(job_result)
             jobs.append(job)
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from indeed import *
+from indeed import get_whole_jobs as get_indeed_jobs
+from linkedin import get_whole_jobs as get_linkedin_jobs
+from save import save_to_file
 
-last_indeed_page = extract_indeed_pages()
+indeed_jobs = get_indeed_jobs()
+linkedin_jobs = get_linkedin_jobs()
 
-indeed_jobs = extract_indeed_jobs(last_indeed_page)
-
-print(indeed_jobs)
+jobs = indeed_jobs + linkedin_jobs
+save_to_file(jobs)
